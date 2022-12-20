f66047c442b4426c21d67af64f4ed3dac562f929
fadiinho <ffadiinho@gmail.com>
2022-12-20 00:48:59 -0300

    fix: remove unused content

--- src/types/errors.d.ts
+++
@@ -1,9 +0,0 @@
-export interface TabnewsError {
-    name: string;
-    message: string;
-    action: string;
-    status_code: number;
-    error_id: string;
-    request_id: string;
-    error_location_code: string;
-}
